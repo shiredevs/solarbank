f8824b660f83159a382865e661bcdeaf015f6382
michael <michael.e.budd@gmail.com>
2024-05-23 18:19:04 +1000

    task/jira-15 springboot files added

---
+++ server/src/main/java/org/solarbank/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package org.solarbank.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
