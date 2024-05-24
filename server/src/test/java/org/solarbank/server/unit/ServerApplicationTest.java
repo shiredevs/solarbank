09ea0a718ac0a91604dcbf85f82d7d4de6942f2e
pholman <pjohnholman@gmail.com>
2024-05-24 15:33:54 +0100

    updates server config

---
+++ server/src/test/java/org/solarbank/server/unit/ServerApplicationTest.java
@@ -0,0 +1,12 @@
+package org.solarbank.server.unit;
+
+import org.junit.jupiter.api.Test;
+import org.solarbank.server.ServerApplication;
+
+class ServerApplicationTest {
+
+    @Test
+    public void executeApplication() {
+        ServerApplication.main(new String[] {});
+    }
+}
