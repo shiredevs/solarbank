883f885eef1c0c7b8594a81b8ba3ca7187c6803e
pholman <pjohnholman@gmail.com>
2024-06-16 19:11:44 +0100

    adds tailwind

--- client/src/components/SplashScreen.tsx
+++ client/src/components/SplashScreen.tsx
@@ -4,7 +4,7 @@ import '../styles/SplashScreen.css';
 function SplashScreen(): React.JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header border-8 border-cyan-800">
         <p>Welcome to Solarbank</p>
       </header>
     </div>
