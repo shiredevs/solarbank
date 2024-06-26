288881fa3cac90b02610138b43019eeb5d66e0b0
pholman <pjohnholman@gmail.com>
2024-06-26 15:34:10 +0100

    updates styling for landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React, { JSX } from 'react';
-import './LandingPage.css';
+import style from './LandingPage.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
@@ -7,10 +7,11 @@ const LandingPage = (): JSX.Element => {
   const navigate: NavigateFunction = useNavigate();
 
   return (
-    <div className="landing-page" role="landing-page-container">
-      <FontAwesomeIcon className="icon" icon={['fas', 'cloud-sun']} aria-hidden={false} />
-      <p>Welcome to Solarbank</p>
-      <button className="button" onClick={() => navigate('/form')}>
+    <div className={style.landingPage} role="landing-page-container">
+      <FontAwesomeIcon className={style.icon} icon={['fas', 'cloud-sun']} aria-hidden={false} />
+      <p className={style.heading}>Solarbank</p>
+      <p className={style.paragraph}>estimate your solar energy savings</p>
+      <button className={style.button} onClick={() => navigate('/form')}>
         begin
       </button>
     </div>
